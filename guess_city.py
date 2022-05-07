d301d1edde87299120e68894c3b2d42c3437ab9c
pavelvasilev <pavelvasilev2304@gmail.com>
2022-05-07 01:05:44 -0400

    Added some files

--- guess_city.py
+++ guess_city.py
@@ -1,11 +1,10 @@
+from locale import normalize
 import random
 from flask import Flask, request
 import logging
 import json
 
-logging.basicConfig(
-    filename="server.log"
-)
+logging.basicConfig()
 
 app = Flask(__name__)
 storage = {}
@@ -14,10 +13,13 @@ questions = {
     '997614/529b016475eb245d1387': 'Париж',
     '1652229/a4179ba6f690906effef': 'Нью-Йорк',
     '997614/c79728cb11e86d75ee94': 'Нью-Йорк',
-    '1533899/0a547d94785e38d6f477': 'Москва',
-    '1521359/1a1ae17f3c8b531490e6': 'Москва',
-    'count': 6
+    '213044/84d0f658561ff56a2e70': 'Москва',
+    'count': 5
 }
+
+
+def normalize_answer(string):
+    return string.replace(' ', '')
 
 
 @app.route("/post", methods=["POST"])
@@ -32,11 +34,11 @@ def handle_request():
     }
     response = make_response(request.json, response)
     logging.info("Maked a response..")
-    return json.dump(response)
+    return json.dumps(response)
     
 
 def make_response(req, res):
-    if 'прив' in req['request']['text']:
+    if req['session']['new']:
         res['response']['text'] = "Привет! Меня зовут Алиса, давай сыграем?"
         res['response']['buttons'] = [
         {
@@ -50,48 +52,64 @@ def make_response(req, res):
         ]
         return res
     
-    if 'да' in req['request']['text']:
+    if "помощь" in req['request']['nlu']['tokens']:
+        res['response']['text'] = \
+        """
+            Чтобы выиграть, вам надо правильно называть города,
+            кстати, сейчас ваш ход: (пишите)
+        """
+        return res
+    
+    if 'да' in req['request']['command']:
         init_game(req['session']['user_id'])
-    elif 'нет' in req['request']['text']:
+    elif 'нет' in req['request']['command']:
         res['response']['text'] = "Раз нет, значит, нет.."
         res['response']['end_session'] = True
         return res
     
     user_id = req['session']['user_id']
     data = storage[user_id]
+    
     if data['cur'] < questions['count']:
+        res['response']['text'] = "some problems.."
         res['response']['card'] = {
-            'type': 'BitImage',
-            'image_id': data[data['cur']],
-            'title': 'Угадайте город!',
-            'buttons': [
-            {
-                'title': 'Москва',
-                'payload': {'answer': 'Москва'}
-            },
-            {
-                'title': 'Париж',
-                'payload': {'answer': 'Париж'}
-            },
-            {
-                'title': 'Нью-Йорк',
-                'payload': {'answer': 'Нью-Йорк'}
-            }]
+            'type': 'BigImage',
+            'image_id': data['q'][data['cur']],
         }
+        res['response']['buttons'] = [
+            {
+                'title': 'Помощь',
+                "payload": {},
+                "hide": True
+            }
+        ]
+        
+        if data['rigth_answer'] != None:
+            is_rigth = req['request']['command'] == data['rigth_answer'].lower()
+            res['response']['card']['title'] = ("Верно!" 
+                                                if is_rigth
+                                                else 'Неверно!')
+            storage[user_id]['result'] += is_rigth
+        data['rigth_answer'] = questions[data['q'][data['cur']]]
+        data['cur'] += 1
         return res
     else:
+        res['response']['text'] = f"Вы прошли! С результатом {data['result']}"
         res['response']['end_session'] = True
         return res
 
 
 def init_game(user_id):
+    list_ = list(questions.keys())
+    random.shuffle(list_)
     storage[user_id] = {
-        'q': random.shaffle(list(questions.keys())),
-        'cur': 0
+        'q': list_,
+        'cur': 0,
+        'rigth_answer': None,
+        'result': 0
     }
     return
 
 
-if __name__ == '__main__':
-    app.run(port=443)
+app.run(port=8080)
 
